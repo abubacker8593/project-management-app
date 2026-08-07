d189845763c026868ff7ba8561477be034077ae4
Abubacker <abubackermax10@gmail.com>
2026-08-07 12:07:12 +0530

    created all folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Routes,Route,Router } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('green')
 
   return (
-    <div>
-     
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+    </>
   )
 }
 
